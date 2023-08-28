05021eb510218b05edd379341ca841bccaf17f21
svdjcuwg4638 <kdhwon1@gmail.com>
2023-08-28 20:46:25 +0900

    cr

--- factorypjf/src/component/management/code/CommonCode.js
+++ factorypjf/src/component/management/code/CommonCode.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { codeAction } from "redux/actions/management/codeAction";
 import api from "redux/api";
 
 const CommonCode = ({ selectId, codeAll }) => {
@@ -6,35 +8,41 @@ const CommonCode = ({ selectId, codeAll }) => {
     (data) => data.management_code_id === selectId
   );
 
+  const tableRef = useRef(null);
+
+  const handleScroll = (e) => {
+    const { deltaY } = e;
+    if (tableRef.current) {
+      tableRef.current.scrollTop += deltaY;
+    }
+  };
+
+  const dispatch = useDispatch();
+
   const [formData, setFormData] = useState({
     code: "",
     name: "",
-    management_code_id:"",
+    management_code_id: "",
   });
 
-  const handleChange =  (e) =>{
-    const {name,value} = e.target
+  const handleChange = (e) => {
+    const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name] : value,
-    })
-    console.log("formdata", formData);
-  }
+      [name]: value,
+      management_code_id: selectId,
+    });
+  };
 
-  const handleSubmit = async (e) =>{
-    e.preventDefault()
-    try{
-      setFormData({
-        ...formData,
-        management_code_id : selectId
-      })
-      const response = await api.post("/managecode/add",formData)
-
-    }catch(error){
-      console.log("error :" , error)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post("/code/add", formData);
+      dispatch(codeAction.getCodeAll());
+    } catch (error) {
+      console.log("error :", error);
     }
-  }
-
+  };
 
   return (
     <div>
@@ -45,7 +53,7 @@ const CommonCode = ({ selectId, codeAll }) => {
             <th>이름</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="scrollable-table" onWheel={handleScroll}>
           {codeAll &&
             filteredData.map((data) => (
               <tr>
@@ -58,10 +66,20 @@ const CommonCode = ({ selectId, codeAll }) => {
       <form className="mt-3" onSubmit={handleSubmit}>
         <tr>
           <td>
-            <input type="text" name="code" value={formData.code} onChange={handleChange}/>
+            <input
+              type="text"
+              name="code"
+              value={formData.code}
+              onChange={handleChange}
+            />
           </td>
           <td>
-            <input type="text" name="name"  value={formData.name} onChange={handleChange}/>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
           </td>
         </tr>
         <div>
--- factorypjf/src/component/management/code/ManageCode.js
+++ factorypjf/src/component/management/code/ManageCode.js
@@ -1,5 +1,43 @@
+import { useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { codeAction } from "redux/actions/management/codeAction";
+import api from "redux/api";
+
 const ManageCode = ({ manageCodeAll, setSelectId, selectId }) => {
-  console.log("selectId", selectId);
+  const dispatch = useDispatch();
+
+  const tableRef = useRef(null);
+
+  const handleScroll = (e) => {
+    const { deltaY } = e;
+    if (tableRef.current) {
+      tableRef.current.scrollTop += deltaY;
+    }
+  };
+
+  const [formData, setFormData] = useState({
+    management_name: "",
+    management_code: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post("/managecode/add", formData);
+      dispatch(codeAction.getCodeAll());
+    } catch (error) {
+      console.log("error :", error);
+    }
+  };
+
   return (
     <div>
       <table>
@@ -9,20 +47,57 @@ const ManageCode = ({ manageCodeAll, setSelectId, selectId }) => {
             <th>관리코드명</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="scrollable-table" onWheel={handleScroll}>
           {manageCodeAll &&
             manageCodeAll.map((data) => (
               <tr onClick={() => setSelectId(data.management_code_id)}>
                 <td
-                style={{backgroundColor: selectId == data.management_code_id ? "#dadada" : "transparent",}}
-                >{data.management_code}</td>
+                  style={{
+                    backgroundColor:
+                      selectId == data.management_code_id
+                        ? "#dadada"
+                        : "transparent",
+                  }}
+                >
+                  {data.management_code}
+                </td>
                 <td
-                style={{backgroundColor: selectId == data.management_code_id ? "#dadada" : "transparent",}}
-                >{data.management_name}</td>
+                  style={{
+                    backgroundColor:
+                      selectId == data.management_code_id
+                        ? "#dadada"
+                        : "transparent",
+                  }}
+                >
+                  {data.management_name}
+                </td>
               </tr>
             ))}
         </tbody>
       </table>
+      <form className="mt-3" onSubmit={handleSubmit}>
+        <tr>
+          <td>
+            <input
+              type="text"
+              name="management_code"
+              value={formData.management_code}
+              onChange={handleChange}
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              name="management_name"
+              value={formData.management_name}
+              onChange={handleChange}
+            />
+          </td>
+        </tr>
+        <div>
+          <button type="submit">추가</button>
+        </div>
+      </form>
     </div>
   );
 };
